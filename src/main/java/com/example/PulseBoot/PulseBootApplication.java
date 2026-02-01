0ec776dfa0ebd6bf764b1a8879a17e74001ac78e
Ria Pandey <riapandey0805@gmail.com>
2026-02-01 12:24:54 +0530

    first commit

---
+++ src/main/java/com/example/PulseBoot/PulseBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.PulseBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseBootApplication.class, args);
+	}
+
+}
